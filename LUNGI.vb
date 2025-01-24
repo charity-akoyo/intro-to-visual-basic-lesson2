3f97decdf1dfca5696146d3668b85dffbb3638aa
charity-akoyo <charityakoyo51@gmail.com>
2025-01-24 12:47:37 +0300

    Add files via upload
    SECOND LESSON

---
+++ LUNGI.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Welcome to VB2019")
+    End Sub
+
+End Class
